0dc13a3c9a5f52c68743546dcac62af0c33a92a7
drakkomaximo <andres.villanueva.tabares@gmail.com>
2025-05-02 17:56:07 -0500

    Ui has been created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,20 @@
 import { HydrateClient } from "~/trpc/server";
 import Modules from "~/app/_components/dashboard_components/Modules"; // fix path if different
+import { HowItWorks } from "~/app/_components/HowItWorks";
 
 export default async function Home() {
   return (
     <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center text-white py-12">
+      <main className="flex min-h-screen flex-col items-center text-black">
         <h1 className="text-5xl font-extrabold tracking-tight mt-10 sm:text-[5rem] mb-12">
           OpenTask
         </h1>
 
-        <Modules />
+        <HowItWorks />
+        
+        <div className="w-full bg-[#1f1f1f] text-white">
+          <Modules />
+        </div>
       </main>
     </HydrateClient>
   );
