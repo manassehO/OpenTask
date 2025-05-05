2930564bb9d15331035d6273ac19ed48ea157558
matthew <ekumamatthew1@gmail.com>
2025-05-05 03:06:19 +0000

    auth_layout

---
+++ src/app/(auth)/password/page.tsx
@@ -0,0 +1,41 @@
+"use client";
+import { useState } from "react";
+import AuthWrapper from "~/app/_components/layout/authWrapper";
+import { CreatePassword } from "~/app/_components/ui/form/CreatePassword";
+
+function Password() {
+  const [otp, setOtp] = useState<number | undefined>(undefined);
+  const [error, setError] = useState(false);
+  const otpLength = 6;
+
+  const handleChange = (value: number | undefined) => {
+    setOtp(value);
+    setError(false);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (otp?.toString().length !== otpLength) {
+      setError(true);
+      return;
+    }
+  };
+
+  return (
+    <div className="flex h-svh w-full items-center justify-center">
+      <AuthWrapper
+        text="Welcome to open task. Sign in with your email or connect a wallet to get started"
+        title="Welcome"
+      >
+        <form onSubmit={handleSubmit} className="flex w-96 flex-col gap-4">
+          <CreatePassword />
+          {/* <button className="rounded-md bg-[#3B82F6] p-4 text-white">
+            Proceed
+          </button> */}
+        </form>
+      </AuthWrapper>
+    </div>
+  );
+}
+
+export default Password;
