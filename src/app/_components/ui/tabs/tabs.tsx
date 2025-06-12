16a0c3846974cf4d6b6a8800942ecdaed0228db2
wheval <whevalmezeegbe@gmail.com>
2025-06-12 13:20:52 +0100

    fix: remove duplicate card-image

--- src/app/_components/ui/tabs/tabs.tsx
+++ src/app/_components/ui/tabs/tabs.tsx
@@ -68,7 +68,7 @@ const TabsTrigger = React.forwardRef<HTMLButtonElement, TabsTriggerProps>(
       <button
         ref={ref}
         className={cn(
-          "inline-flex items-center justify-center whitespace-nowrap rounded-[4px] px-3 py-3 text-sm text-[#414141] font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+          "inline-flex items-center justify-center whitespace-nowrap rounded-[4px] px-2 py-2 text-sm text-[#414141] font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
           isActive && "bg-background bg-[#3B82F6] text-white shadow-sm",
           className
         )}
