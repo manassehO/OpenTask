eac4ad2ba54fa32c3a05356aef16a5d5cc96390a
Ajibose <ajiboseibrahim12@gmail.com>
2025-07-21 13:57:25 +0100

    Revert changes in starknetSVC

--- src/services/starknetSvc.ts
+++ src/services/starknetSvc.ts
@@ -1,3 +1,35 @@
+const STARKNET_RPC_URL = process.env.STARKNET_RPC_URL;
+
+export const deployAAWallet = async (userId: string) => {
+  console.log(`[StarknetSvc] Request to deploy AA Wallet for user: ${userId}`);
+  console.log(`[StarknetSvc] Using RPC endpoint: ${STARKNET_RPC_URL}`);
+  //Interact with chipi
+};
+
+export const approve = () => {
+  console.log('Stub: Approving transaction');
+};
+
+export const fundTask = () => {
+  console.log('Stub: Funding task');
+};
+
+export const transfer = () => {
+  console.log('Stub: Transferring tokens');
+};
+
+export const verifyMessage = () => {
+  console.log('Stub: Verifying message');
+};
+
+export const flagDispute = () => {
+  console.log('Stub: Flagging dispute');
+};
+
+export const resolveDispute = () => {
+  console.log('Stub: Resolving dispute');
+};
+
 export const fundTaskWithManagedWallet = async ({
   taskId,
   totalFunding,
