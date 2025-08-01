374dc43899476c01d34ff4c45ff1ba181dafb87e
Ikem Peter <ikempeter2020@gmail.com>
2025-08-01 14:56:03 +0100

    chore: remove drizzle migration files

--- drizzle/0004_complex_vapor.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "opentask_task_claims" ALTER COLUMN "user_id" SET DATA TYPE text;--> statement-breakpoint
-ALTER TABLE "opentask_task_claims" ALTER COLUMN "updated_at" SET DEFAULT now();--> statement-breakpoint
-ALTER TABLE "opentask_tasks" ADD COLUMN "max_completions" integer NOT NULL;
--- drizzle/0012_nice_inhumans.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "opentask_verification" ALTER COLUMN "id" SET DATA TYPE text;--> statement-breakpoint
-ALTER TABLE "opentask_verification" ALTER COLUMN "id" DROP DEFAULT;
