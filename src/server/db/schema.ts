151c388e47af5e0f660c83ab3fe4b7b14770a853
Ajibose <ajiboseibrahim12@gmail.com>
2025-08-05 08:48:24 +0100

    Fixed broken features on the backend

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -15,7 +15,6 @@ import {
   numeric,
   bigint,
 } from 'drizzle-orm/pg-core';
-import { relations } from 'drizzle-orm';
 
 export const createTable = pgTableCreator((name) => `opentask_${name}`);
 const statusEnum = pgEnum('status', ['ACTIVE', 'SUSPENDED', 'BANNED']);
