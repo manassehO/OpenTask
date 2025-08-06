3e93ba366f888b7ee4b3fb75a5c6827d8c12d534
Kokeh <kokehinc@gmail.com>
2025-08-06 18:14:42 +0100

    feat: add learning router with courses and tutorials endpoints

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -3,6 +3,7 @@ import { authRouter } from '~/server/api/routers/auth/auth';
 import { profileRouter } from '~/server/api/routers/profile';
 import { walletRouter } from './routers/wallet';
 import { taskRouter } from './routers/task';
+import { learningRouter } from './routers/learning';
 
 /*
  * This is the primary router for your server.
@@ -14,6 +15,7 @@ export const appRouter = createTRPCRouter({
   profile: profileRouter,
   wallet: walletRouter,
   task: taskRouter,
+  learning: learningRouter,
 });
 
 // export type definition of API
