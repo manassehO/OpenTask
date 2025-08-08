ec657d39341067050030f24b43fb7a22957ef156
Ajibose <ajiboseibrahim12@gmail.com>
2025-08-08 15:48:05 +0100

    Implemeted global search across task, courses, and tutorials

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -4,6 +4,7 @@ import { profileRouter } from '~/server/api/routers/profile';
 import { walletRouter } from './routers/wallet';
 import { taskRouter } from './routers/task';
 import { adminRouter } from './routers/admin';
+import { searchRouter } from './routers/search';
 
 /*
  * This is the primary router for your server.
@@ -16,6 +17,7 @@ export const appRouter = createTRPCRouter({
   wallet: walletRouter,
   task: taskRouter,
   admin: adminRouter,
+  search: searchRouter,
 });
 
 // export type definition of API
