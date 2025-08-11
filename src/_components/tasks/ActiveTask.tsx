30e05d2594fbfc3ecaeb040a119100d85c7baf3c
best2025j <audugodstime95@gmail.com>
2025-08-11 20:41:59 +0100

    feat: still working on api endpoint

--- src/_components/tasks/ActiveTask.tsx
+++ src/_components/tasks/ActiveTask.tsx
@@ -1,6 +1,7 @@
 import { mockTasks } from '~/mocks/tasks';
 import TaskCard from './TaskCard';
 import { useRouter } from 'next/navigation';
+import { getTaskById } from '~/app/api/task';
 
 export default function ActiveTask() {
   const router = useRouter();
@@ -8,6 +9,13 @@ export default function ActiveTask() {
   const onClick = (taskId: string) => {
     router.push(`/task/${taskId}`);
   };
+
+  const { data, isLoading, error } = getTaskById(
+    'e146ae52-0d8e-4760-b5d8-3d5b38b412d4',
+  );
+  console.log(data);
+  console.log(error);
+  console.log(isLoading);
 
   return (
     <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
